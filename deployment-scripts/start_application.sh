7b3799ae65c9eb7f6535f1e34c14bfe1893d00d7
Volodymyr Sapsai <vsapsai@gmail.com>
2017-06-20 23:22:52 -0700

    Make start_application.sh copy different .war files to different contexts.

--- deployment-scripts/start_application.sh
+++ deployment-scripts/start_application.sh
@@ -3,29 +3,31 @@
 set -e
 
 CATALINA_BASE='/var/lib/tomcat8'
-DEPLOY_TO_ROOT='true'
-#CONTEXT_PATH='##CONTEXT_PATH##'
 SERVER_HTTP_PORT='80'
 
 TEMP_STAGING_DIR='/tmp/codedeploy-deployment-staging-area'
-WAR_STAGED_LOCATION="$TEMP_STAGING_DIR/general.war"
 HTTP_PORT_CONFIG_XSL_LOCATION="$TEMP_STAGING_DIR/configure_http_port.xsl"
 
-# In Tomcat, ROOT.war maps to the server root
-if [[ "$DEPLOY_TO_ROOT" = 'true' ]]; then
-    CONTEXT_PATH='ROOT'
-fi
+# Parameters are:
+#   $1 - .war file name
+#   $2 - context name to use
+copy_war_file_to_context() {
+    WAR_STAGED_LOCATION="$TEMP_STAGING_DIR/$1"
+    CONTEXT_PATH="$2"
 
-# Remove unpacked application artifacts
-if [[ -f $CATALINA_BASE/webapps/$CONTEXT_PATH.war ]]; then
-    rm $CATALINA_BASE/webapps/$CONTEXT_PATH.war
-fi
-if [[ -d $CATALINA_BASE/webapps/$CONTEXT_PATH ]]; then
-    rm -rfv $CATALINA_BASE/webapps/$CONTEXT_PATH
-fi
+    # Remove unpacked application artifacts
+    if [[ -f $CATALINA_BASE/webapps/$CONTEXT_PATH.war ]]; then
+        rm $CATALINA_BASE/webapps/$CONTEXT_PATH.war
+    fi
+    if [[ -d $CATALINA_BASE/webapps/$CONTEXT_PATH ]]; then
+        rm -rfv $CATALINA_BASE/webapps/$CONTEXT_PATH
+    fi
 
-# Copy the WAR file to the webapps directory
-cp $WAR_STAGED_LOCATION $CATALINA_BASE/webapps/$CONTEXT_PATH.war
+    # Copy the WAR file to the webapps directory
+    cp $WAR_STAGED_LOCATION $CATALINA_BASE/webapps/$CONTEXT_PATH.war
+}
+
+copy_war_file_to_context "general.war" "ROOT"
 
 # Configure the Tomcat server HTTP connector
 { which xsltproc; } || { apt-get install -y xsltproc; }
