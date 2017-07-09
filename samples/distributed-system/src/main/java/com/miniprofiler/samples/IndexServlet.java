007c281672768b4b8ad6afc3a7f5741031ac5c3f
Volodymyr Sapsai <vsapsai@gmail.com>
2017-07-08 20:01:43 -0700

    [DistributedSystems] Get Wikipedia article from our service, not directly.
    There is an option to use Jersey client but I decided to stick to Apache
    HttpClient for now to keep it general and independent from the service
    implementation.

--- samples/distributed-system/src/main/java/com/miniprofiler/samples/IndexServlet.java
+++ samples/distributed-system/src/main/java/com/miniprofiler/samples/IndexServlet.java
@@ -1,10 +1,6 @@
 package com.miniprofiler.samples;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.ServletException;
@@ -12,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.commons.io.IOUtils;
@@ -26,25 +21,24 @@ import com.miniprofiler.Closeable;
 import com.miniprofiler.MiniProfiler;
 
 public class IndexServlet extends HttpServlet {
-    private static String WIKIPEDIA_REQUEST_URL =
-            "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json";
-    private static String WIKIPEDIA_ARTICLE_URL = "https://en.wikipedia.org/wiki/";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Optional<String> randomArticleTitle = getRandomWikipediaArticleName();
-        request.setAttribute("article", buildArticleRepresentation(randomArticleTitle));
+        String serverUrl = getServerUrl(request);
+        Optional<WikipediaArticle> randomArticle = getRandomWikipediaArticle(serverUrl);
+        String articleUrl = randomArticle.map(this::buildArticleUrl).orElse("Request failed");
+        request.setAttribute("article", articleUrl);
         request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
     }
 
-    private Optional<String> getRandomWikipediaArticleName() throws IOException {
+    private Optional<WikipediaArticle> getRandomWikipediaArticle(String serverUrl) throws IOException {
         MiniProfiler profiler = MiniProfiler.getCurrent();
-        // Get response from Wikipedia.
+        // Get response from distributed-system-service.
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpGet httpGet = new HttpGet(WIKIPEDIA_REQUEST_URL);
+        HttpGet httpGet = new HttpGet(serverUrl + "/distributed-system-service/rest/wikipedia/article/random");
         String responseContent = "";
-        try (Closeable step = profiler.step("Call Wikipedia");
+        try (Closeable step = profiler.step("distributed-system-service");
              CloseableHttpResponse response = httpClient.execute(httpGet)) {
             if (response.getStatusLine().getStatusCode() != HttpServletResponse.SC_OK) {
                 return Optional.empty();
@@ -55,61 +49,26 @@ public class IndexServlet extends HttpServlet {
         }
         // Parse response.
         ObjectMapper mapper = new ObjectMapper();
-        WikipediaResponse wikipediaResponse = mapper.readValue(responseContent, WikipediaResponse.class);
-        return Optional.of(wikipediaResponse.query.random.get(0).title);
+        WikipediaArticle article = mapper.readValue(responseContent, WikipediaArticle.class);
+        return Optional.of(article);
     }
 
-    private String buildArticleRepresentation(Optional<String> articleTitle) {
-        return articleTitle
-                .map(title -> {
-                    String url = null;
-                    try {
-                        url = WIKIPEDIA_ARTICLE_URL + URLEncoder.encode(title.replace(' ', '_'),
-                                StandardCharsets.UTF_8.name());
-                    } catch (UnsupportedEncodingException e) {
-                        return null;
-                    }
-                    return String.format("<a href=\"%s\">%s</a>", url, StringEscapeUtils.escapeHtml4(title));
-                })
-                .orElse("Request failed");
+    private String getServerUrl(HttpServletRequest request) {
+        StringBuffer url = new StringBuffer()
+            .append(request.getScheme())
+            .append("://")
+            .append(request.getServerName())
+            .append(":")
+            .append(request.getServerPort());
+        return url.toString();
     }
 
-    /*
-        {
-            "batchcomplete": "",
-            "continue": {
-                "rncontinue": "0.284646709280|0.284647818375|31090907|0",
-                "continue": "-||"
-            },
-            "query": {
-                "random": [
-                    {
-                        "id": 53693569,
-                        "ns": 0,
-                        "title": "First National Bank of Ekalaka and Rickard Hardware Store Building"
-                    }
-                ]
-            }
-        }
-    */
-    private static class WikipediaResponse {
-        static class ContinuationToken {
-            @JsonProperty("rncontinue") String randomContinueToken;
-            @JsonProperty("continue") String continueToken;
-        }
+    private String buildArticleUrl(WikipediaArticle article) {
+        return String.format("<a href=\"%s\">%s</a>", article.url, StringEscapeUtils.escapeHtml4(article.title));
+    }
 
-        static class Item {
-            @JsonProperty("id") long id;
-            @JsonProperty("ns") long nameSpace;
-            @JsonProperty("title") String title;
-        }
-
-        static class Query {
-            @JsonProperty("random") List<Item> random;
-        }
-
-        @JsonProperty("batchcomplete") String batchComplete;
-        @JsonProperty("continue") ContinuationToken continuationToken;
-        @JsonProperty("query") Query query;
+    private static class WikipediaArticle {
+        public String title;
+        public String url;
     }
 }
